e3908cdcd8bb061e2c9aa65876aa231794b18e71
Anna Silva <git@notjustanna.net>
2026-05-22 10:36:53 -0300

    Set site URL to receitas.notjustanna.net

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://receitas.notjustanna.net',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
